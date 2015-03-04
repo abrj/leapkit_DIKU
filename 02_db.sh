514a1894003cc8093555f75379acc5f16af22cc8
Martin J <martinnjorgensen@gmail.com>
2015-03-04 12:34:56 +0100

    Update 02_db.sh

--- 02_db.sh
+++ 02_db.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
+# Change to the correct working directory
+cd leapkit/
 
+# Sync the database model (Django analyses classes and creates it's own models.)
 ./manage.py syncdb
+
+# Migrate the created models to the SQL database.
 ./manage.py migrate
+
+# Some models won't migrate because of relations, migrate in specific order to fix.
 ./manage.py migrate geographic_info
 ./manage.py migrate institutions
 ./manage.py migrate projects
