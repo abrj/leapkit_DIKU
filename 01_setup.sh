f73c4d6821e21c47e4ebeee3f06965142fee8aaa
Martin J <martinnjorgensen@gmail.com>
2015-03-04 12:32:11 +0100

    Update 01_setup.sh
    All "apt-get install" calls are in one line, only one interaction requred.
    Added comments.

--- 01_setup.sh
+++ 01_setup.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
 
-# Install relevent packages
-
+# update base system.
 sudo apt-get update
 sudo apt-get upgrade
 sudo apt-get dist-upgrade
+
+# Install dependencies
 sudo apt-get build-dep python-imaging
-sudo apt-get install vim emacs
-sudo apt-get install libjpeg62 libjpeg62-dev
+sudo apt-get install vim emacs libjpeg62 libjpeg62-dev python-pip python-psycopg2 postgresql
+
+# Do magic linking.
 sudo ln -s /usr/lib/i386-linux-gnu/libz.so /usr/lib/libz.so
 sudo ln -s /usr/lib/i386-linux-gnu/libjpeg.so /usr/lib/libjpeg.so
 sudo ln -s /usr/lib/i386-linux-gnu/libfreetype.so /usr/liblibfreetype.so
-sudo apt-get install python-pip
 
+# Install leapkit specific packages from pip.
 sudo pip install -r requirements.txt
 
+# Change to correct working directory and apply django settings.
 cd leapkit
 cp leapkit/settings/base_settings_local.py leapkit/settings/base_settings.py
-sudo apt-get install python-psycopg2
-sudo apt-get install postgresql
